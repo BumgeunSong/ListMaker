b53c668c0393f7a10453a76cf3c2e74003d93035
BumgeunSong <isp1195@gmail.com>
2024-02-17 20:45:04 +0900

    잘못 들어가있던 modifier 수정

--- app/src/main/java/com/eddy/listmaker/views/ListItemView.kt
+++ app/src/main/java/com/eddy/listmaker/views/ListItemView.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.eddy.listmaker.Greeting
@@ -32,10 +33,14 @@ fun ListItemView(value: String, onClick: (String) -> Unit) {
             .padding(6.dp)
             .clickable { onClick(clickedItem) },
         shape = shape,
-        color = MaterialTheme.colorScheme.background.copy(alpha = 0.3f)
+        color = MaterialTheme.colorScheme.background
     ) {
         Column(modifier = Modifier.padding(10.dp)) {
-            Text(text = value, modifier = Modifier.size(4.dp))
+            Text(
+                text = value,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.padding(4.dp)
+            )
         }
         Spacer(modifier = Modifier.size(8.dp))
     }
