a9ff4e57f58e0b95ceb17be44a0462c8456d0c75
BumgeunSong <isp1195@gmail.com>
2024-02-17 20:36:48 +0900

    Scaffold로 topAppBar, 플로팅버튼, 리스트 연결

--- app/src/main/java/com/eddy/listmaker/views/TaskListContent.kt
+++ app/src/main/java/com/eddy/listmaker/views/TaskListContent.kt
@@ -1,10 +1,44 @@
 package com.eddy.listmaker.views
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.eddy.listmaker.R
 import com.eddy.listmaker.data.TaskList
+
+@Composable
+fun TaskListScreen() {
+    Scaffold(
+        topBar = {
+            ListMakerTopAppBar(
+                title = stringResource(id = R.string.label_list_maker),
+                showBackButton = false,
+                onBackPressed = {}
+            )
+        },
+        content = {
+            TaskListContent(
+                modifier = Modifier.padding(it).fillMaxSize(),
+                tasks = emptyList(),
+                onClick = { taskName ->
+                    println(taskName)
+                })
+        },
+        floatingActionButton = {
+            ListMakerFloatingActionButton(
+                title = stringResource(id = R.string.name_of_list),
+                inputHint = stringResource(id = R.string.task_hint),
+                onFabPressed = {
+                    println(it)
+                }
+            )
+        }
+    )
+}
 
 @Composable
 fun TaskListContent(
