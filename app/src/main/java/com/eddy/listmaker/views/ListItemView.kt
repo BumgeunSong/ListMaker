702df227d199c010c1cfba1bc67050a01a499d12
BumgeunSong <isp1195@gmail.com>
2024-02-18 11:19:08 +0900

    기본 Greeting 삭제

--- app/src/main/java/com/eddy/listmaker/views/ListItemView.kt
+++ app/src/main/java/com/eddy/listmaker/views/ListItemView.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.eddy.listmaker.Greeting
 import com.eddy.listmaker.ui.theme.ListMakerTheme
 
 @Composable
