0f2bf1e5f43bccf33f99d0a8bf822a2199c61b78
BumgeunSong <isp1195@gmail.com>
2024-02-18 10:54:15 +0900

    TaskList를 로컬에서 읽어오는 함수 만들기

--- app/src/main/java/com/eddy/listmaker/viewModel/TaskListManager.kt
+++ app/src/main/java/com/eddy/listmaker/viewModel/TaskListManager.kt
@@ -10,4 +10,16 @@ class TaskListManager(val context: Context) {
         sharedPreferences.putStringSet(list.name, list.tasks.toHashSet())
         sharedPreferences.apply()
     }
+
+    fun readLists(): ArrayList<TaskList> {
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val contents = sharedPrefs.all
+        val taskLists = ArrayList<TaskList>()
+        for (taskList in contents) {
+            val tasks = ArrayList(taskList.value as HashSet<String>)
+            val list = TaskList(name = taskList.key, tasks = tasks)
+            taskLists.add(list)
+        }
+        return taskLists
+    }
 }
